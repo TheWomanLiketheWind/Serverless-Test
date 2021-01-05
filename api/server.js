b4c6cabd3a166d18aaa6bfaf08354e5b3211a2e8
高巧霞 <gaoqiaoxia@gaoqiaoxiadeMacBook-Pro.local>
2021-01-05 14:08:09 +0800

    修改路径

--- api/server.js
+++ api/server.js
@@ -1,8 +1,7 @@
 // server.js
-const path = require('path')
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname), '../data.json')
+const router = jsonServer.router('data.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
